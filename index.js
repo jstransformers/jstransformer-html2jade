5d640a4c86fbac72698d4e6d376551c14045358f
Timothy Gu <timothygu99@gmail.com>
2015-08-06 13:57:32 +0800

    Use Bluebird.promisify

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 'use strict';
 
 var Bluebird = require('bluebird');
-var html2jade = require('html2jade');
+var convert = Bluebird.promisify(require('html2jade').convertHtml);
 
 exports.name = 'html2jade';
 exports.inputFormats = ['html', 'html2jade'];
 exports.outputFormat = 'jade';
 
-exports.renderAsync = function (str, options) {
-  options = options && typeof options === 'object' ? options : options;
-
-  return new Bluebird(function (resolve, reject) {
-    html2jade.convertHtml(str, options, function (err, jade) {
-      if (err) {
-        return reject(err);
-      }
-      return resolve(jade);
-    });
-  });
+exports.renderAsync = function (str, options, locals) {
+  return convert(str, options);
 };
