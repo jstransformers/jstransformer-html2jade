596a29c0a4e52b18f46c5d02560aac23b96204e5
Rob Loach <robloach@gmail.com>
2015-11-05 09:35:32 -0500

    Update Boilerplate

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 'use strict';
 
-var Bluebird = require('bluebird');
-var convert = Bluebird.promisify(require('html2jade').convertHtml);
+var Promise = require('promise');
+var html2jade = require('html2jade');
 
 exports.name = 'html2jade';
 exports.inputFormats = ['html', 'html2jade'];
 exports.outputFormat = 'jade';
 
 exports.renderAsync = function (str, options) {
-  return convert(str, options);
+  return new Promise(function (resolve, reject) {
+    html2jade.convertHtml(str, options, function (err, jade) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(jade);
+      }
+    });
+  });
 };
