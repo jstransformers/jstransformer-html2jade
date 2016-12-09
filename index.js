97f3a24f4f32f310316477f0b54b0d06337c2a67
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-'use strict';
+'use strict'
 
-var Promise = require('promise');
-var html2jade = require('html2jade');
+var Promise = require('promise')
+var html2jade = require('html2jade')
 
-exports.name = 'html2jade';
-exports.inputFormats = ['html', 'html2jade'];
-exports.outputFormat = 'jade';
+exports.name = 'html2jade'
+exports.inputFormats = ['html', 'html2jade']
+exports.outputFormat = 'jade'
 
 exports.renderAsync = function (str, options) {
   return new Promise(function (resolve, reject) {
     html2jade.convertHtml(str, options, function (err, jade) {
       if (err) {
-        reject(err);
+        reject(err)
+      } else {
+        resolve(jade)
       }
-      else {
-        resolve(jade);
-      }
-    });
-  });
-};
+    })
+  })
+}
